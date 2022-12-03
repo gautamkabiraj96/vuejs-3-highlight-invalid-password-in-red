b9d9e048ea5ba26cdc17aab2700114c935c2823c
Gautam Kabiraj <51134142+gautamkabiraj96@users.noreply.github.com>
2022-12-03 23:54:13 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# vuejs-3-highlight-invalid-password-in-red
-Created with CodeSandbox
+# Highlight invalid password with red border in Vue.js 3 | Example
+
+Learn how to highlight the invalid field or password field with a red border by checking it with keyup events in Vue.js 3 with demo example. [Learn more](https://www.nightprogrammer.com/vue-js/highlight-invalid-password-with-red-border-in-vue-js-3-example).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { ref } from 'vue';
+
+const isInputInvalid = ref(false);
+const inputPasswordValue = ref('');
+
+const checkInputValidity = () => {
+    isInputInvalid.value = inputPasswordValue.value.length < 8
+}
+</script>
+
+<template>
+<div>
+    <h2>
+      Enter password
+    </h2>
+    <input 
+      type="password" 
+      :class="{
+        'input-password': true, 
+        'invalid-password': isInputInvalid
+      }"
+      v-model="inputPasswordValue" 
+      @keyup="checkInputValidity" />
+    <p v-if="isInputInvalid">
+      Password should be at least 8 characters long!
+    </p>
+</div>
+</template>
+
+<style scoped>
+.input-password {
+    font-size: 16px;
+    letter-spacing: 3px;
+    padding: 4px 6px;
+    outline: none;
+    border: 1px solid #000;
+}
+
+.invalid-password {
+  border: 1px solid red;
+}
+</style>
